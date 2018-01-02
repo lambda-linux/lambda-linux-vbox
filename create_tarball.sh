a4b21ece5dd97273daf8a45e2ed4b5e22126c548
Rajiv M Ranganath <rajiv.ranganath@atihita.com>
2018-01-02 12:26:17 +0000

    Remove non-alpine artifacts

--- create_tarball.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-git archive --format=tar --prefix=lambda-linux-vbox/ --output=../lambda-linux-vbox.tar master
--- install-root/etc-lvm-profile-docker--docker-pool-extend.profile
+++
@@ -1,5 +0,0 @@
-activation {
-	thin_pool_autoextend_threshold=60
-	thin_pool_autoextend_percent=20
-
-}
